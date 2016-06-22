c151a995172e3ac2540ba328d414c64994c04a58
tang261547902 <261547902@qq.com>
2016-06-22 15:01:23 +0800

    test111

--- testpy.py
+++ testpy.py
@@ -1,11 +1,14 @@
-﻿#coding: utf-8
+﻿# coding: utf-8
 import math as mm
+
+
 class tt:
-	def t1():
-		a=input('test input')
-		b=mm.sqrt(float(a))
-		print(b)
-if __name__='__main__':
-	c=tt()
-	c.t1()
-	
+    def t1(self):
+        a = input('test input:')
+        b = mm.sqrt(float(a))
+        print(b)
+
+
+if __name__ == '__main__':
+    c = tt()
+    c.t1()
